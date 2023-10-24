6b1a3a688bbb8fc531b18e2a354dbf10721c5095
MMamajonov <mamajonov_muhriddin@mail.ru>
2023-10-24 23:23:57 +0600

    Task

--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -4,11 +4,19 @@ import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class AppConfig {
- 
-    @Bean(name="helloworld")
+
+
+    @Bean(name = "helloworld")
     public HelloWorld getHelloWorld() {
         HelloWorld helloWorld = new HelloWorld();
         helloWorld.setMessage("Hello World!");
         return helloWorld;
+
+    }
+
+    @Bean
+    @Scope("prototype")
+    public POJO.Cat cat() {
+        return new POJO.Cat();
     }
 }
