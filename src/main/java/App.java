c7ebd3b5604b0a498d260a9bafad1e0a5a25aa97
MMamajonov <mamajonov_muhriddin@mail.ru>
2023-10-26 17:24:16 +0600

    Task

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -14,7 +14,7 @@ public class App {
 
         POJO.Cat one = applicationContext.getBean(POJO.Cat.class);
         POJO.Cat two = applicationContext.getBean(POJO.Cat.class);
-        System.out.println("Сравнение бинов helloWorld: " + (two == one));
+        System.out.println("Сравнение бинов helloWorld: " + (tree == leaf));
         System.out.println("Сравнение бинов cat: " + (two == one));
 
         applicationContext.close();
